7bfd2099aea7c1a5f501a51273d965184bae681c
Orozco1606 <m.orozco1606@gmail.com>
2026-02-13 09:40:51 +0100

    Implementada funcion resta

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Calculadora Empresa v1");
         System.out.println("Función suma implementada");
+        System.out.println("Función resta implementada");
     }
 
 }
